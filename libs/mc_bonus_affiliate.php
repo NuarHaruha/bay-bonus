129385b7e8f0dd2e75a23ba3ad438a45fe96ad2c
nuarharuha <nhnoah@gmail.com>
2012-12-07 04:49:48 +0800

    +added libs/mc_bonus_affiliate.php

---
+++ libs/mc_bonus_affiliate.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *  registration bonus
+ *
+ * @package     isralife
+ * @category    bonus
+ *
+ * @author      Nuarharuha
+ * @copyright   Copyright (C) 2012, Nuarharuha, MDAG Consultancy
+ * @license     http://mdag.mit-license.org/ MIT License
+ * @filesource  http://code.mdag.my/baydura_isralife/src
+ * @version     0.1
+ * @access      public
+ */
